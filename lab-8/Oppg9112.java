33ebbdfc0712b19999ca81c05ce1c063e6054a69
huxflux <johnis@student.hist.no>
2013-10-12 11:50:09 +0200

    bubble bobble

--- lab-8/Oppg9112.java
+++ lab-8/Oppg9112.java
@@ -16,24 +16,24 @@ public class Oppg9112 {
 		double[] jegErLei = new double[DAYS_IN_MONTH];
 		jegErLei = tempShit.middelTempHverDag();
 
-		for (int i = 1; i < DAYS_IN_MONTH+1; i++) {
-			System.out.println("Dag " + i + ": " + jegErLei[i-1]);
+		for (int i = 0; i < DAYS_IN_MONTH; i++) {
+			System.out.printf("Dag " + (i+1) + ": %.2f\n", jegErLei[i]);
 		}
 
 		jegErLei = tempShit.middelTempHverTimeHverDag();
 
-		for (int i = 1; i < HOURS_IN_DAY+1; i++) {
-			System.out.println("Time " + i + ": " + jegErLei[i-1]);
+		for (int i = 0; i < HOURS_IN_DAY; i++) {
+			System.out.printf("Time " + (i+1) + ": %.2f\n", jegErLei[i]);
 		}
 
-		System.out.println("\nMiddeltemperatur for hele maaneden: " + tempShit.middelTempMaaned() + "\n");
+		System.out.printf("\nMiddeltemperatur for hele maaneden: %.2f\n", tempShit.middelTempMaaned());
 
 		jegErLei = tempShit.middelTempGruppert();
 
-		System.out.println("Antall dager under -5 grader: " + jegErLei[0] + " dager");
-		System.out.println("Antall dager mellom -5 og 0 grader: " + jegErLei[1] + " dager");
-		System.out.println("Antall dager mellom 0 og 5 grader: " + jegErLei[2] + " dager");
-		System.out.println("Antall dager mellom 5 og 10 grader: " + jegErLei[3] + " dager");
-		System.out.println("Antall dager over 10: " + jegErLei[4] + " dager");
+		System.out.println("\nAntall dager under -5 grader: " + (int)jegErLei[0] + " dager");
+		System.out.println("Antall dager mellom -5 og 0 grader: " + (int)jegErLei[1] + " dager");
+		System.out.println("Antall dager mellom 0 og 5 grader: " + (int)jegErLei[2] + " dager");
+		System.out.println("Antall dager mellom 5 og 10 grader: " + (int)jegErLei[3] + " dager");
+		System.out.println("Antall dager over 10: " + (int)jegErLei[4] + " dager");
 	}
 }
